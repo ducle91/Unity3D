45c66e3e9062279dafd689c51b1ec6e7d883920c
ducle91 <ducle1991@gmail.com>
2018-02-21 00:51:04 -0500

    added player control

--- rollABall/Assets/script/playerMovement.cs
+++ rollABall/Assets/script/playerMovement.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour {
+public class playerMovement : MonoBehaviour
+{
+	public float speed;
 
-	// Use this for initialization
-	void Start () {
-		
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> (); 
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+
 	}
 }
