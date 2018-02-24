ae658a57c3e3e20e715d859c1dde7fe136b7d83c
ducle91 <ducle1991@gmail.com>
2018-02-24 00:26:12 -0500

    added game objects(coins) and script for collecting coins

--- rollABall/Assets/scripts/playerMovement.cs
+++ rollABall/Assets/scripts/playerMovement.cs
@@ -20,6 +20,13 @@ public class playerMovement : MonoBehaviour
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rb.AddForce (movement * speed);
+	}
 
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.CompareTag ("pickup")) 
+		{
+			other.gameObject.SetActive (false);
+		}
 	}
 }
